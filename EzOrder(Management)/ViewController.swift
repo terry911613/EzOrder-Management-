895cb2ab87f6cb76f5aab1759fc005cba08c1cde
TaiYiLee <terry911613@gmail.com>
2019-05-15 23:57:33 +0800

    Initial Commit

---
+++ EzOrder(Management)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EzOrder(Management)
+//
+//  Created by 李泰儀 on 2019/5/15.
+//  Copyright © 2019 TerryLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
