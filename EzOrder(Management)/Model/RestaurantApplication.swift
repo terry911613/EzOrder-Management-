2f2ace7b718cca47599edadca2b44d80bc92d593
Ken <ken_60401@yahoo.com.tw>
2019-06-10 17:24:35 +0800

    推RestaurantApplicaition

---
+++ EzOrder(Management)/Model/RestaurantApplication.swift
@@ -0,0 +1,13 @@
+//
+//  RestaurantApplication.swift
+//  EzOrder(Management)
+//
+//  Created by Lee Chien Kuan on 2019/6/6.
+//  Copyright © 2019 TerryLee. All rights reserved.
+//
+
+import Foundation
+class RestaurantApplication {
+    var name: String? = "取得紀錄時發生錯誤"
+    var result: Bool?
+}
