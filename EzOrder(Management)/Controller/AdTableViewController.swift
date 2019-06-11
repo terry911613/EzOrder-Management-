ab80779795ee02ebf3e166a8fa95413806bd0f69
Ken <ken_60401@yahoo.com.tw>
2019-06-11 15:28:55 +0800

    management 拉好UI

--- EzOrder(Management)/Controller/AdTableViewController.swift
+++ EzOrder(Management)/Controller/AdTableViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class AdTableViewController: UITableViewController {
 
+    var Ads = ["Ad1"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("2")
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -24,23 +26,23 @@ class AdTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return Ads.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "adCell", for: indexPath) as! AdTableViewCell
+        cell.RestaurantNameLabel.text = Ads[indexPath.row]
         // Configure the cell...
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
